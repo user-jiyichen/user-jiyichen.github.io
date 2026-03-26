95ecb60fecea45210a44abf082dd0d281304dfaa
Yichen Ji <64758590+user-jiyichen@users.noreply.github.com>
2026-03-26 03:30:22 -0400

    Exclude dark mode toggle button from navigation

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -6,7 +6,7 @@
 */
 
 var $nav = $('#site-nav');
-var $btn = $('#site-nav button');
+var $btn = $('#site-nav button:not(.dark-mode-toggle)');
 var $vlinks = $('#site-nav .visible-links');
 var $hlinks = $('#site-nav .hidden-links');
 
